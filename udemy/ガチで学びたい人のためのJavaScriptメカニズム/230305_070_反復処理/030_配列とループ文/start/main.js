e9bccb6fe40ab5375d7c46a1a44a0453fcf4edf5
akiko_kuroda <hahnecochito@gmail.com>
2023-03-06 01:23:33 +0900

    セクション9 反復処理1

--- udemy/ガチで学びたい人のためのJavaScriptメカニズム/230305_070_反復処理/030_配列とループ文/start/main.js
+++ udemy/ガチで学びたい人のためのJavaScriptメカニズム/230305_070_反復処理/030_配列とループ文/start/main.js
@@ -1,1 +1,15 @@
 const arry = [1, 2, 3, 4, 5];
+
+// 配列の値を１つずつconsoleに出力する
+for(let i = 0; i < arry.length; i++) {
+  // const result = i * 2;
+  // console.log(result);
+  console.log(arry[i]);
+}
+
+let v, i = 0;
+// let v;
+// let i = 0;
+while(v < arry[i++]) {
+  console.log(v); // truesyな値の場合はループを続ける
+}
