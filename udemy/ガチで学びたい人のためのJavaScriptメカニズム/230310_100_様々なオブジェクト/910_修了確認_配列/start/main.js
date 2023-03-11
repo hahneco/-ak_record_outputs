8305fdb36f54ad7fae44ae29521478b62b923056
akiko_kuroda <hahnecochito@gmail.com>
2023-03-11 09:25:31 +0900

    「100_様々なオブジェクト」_「930_修了確認_データストア１」まで

--- udemy/ガチで学びたい人のためのJavaScriptメカニズム/230310_100_様々なオブジェクト/910_修了確認_配列/start/main.js
+++ udemy/ガチで学びたい人のためのJavaScriptメカニズム/230310_100_様々なオブジェクト/910_修了確認_配列/start/main.js
@@ -1,23 +1,66 @@
 /**
  * 問題：
  * Arrayを継承して以下のメソッドを実装してみましょう。
- * 
+ *
  * push(*1)
  * forEach
  * map
  * filter
  * reduce
- * 
+ *
  * *1:pushはチェーンメソッドとしてつなげられるように実装してみてください。
  */
 class MyArray extends Array {
 	constructor(...args) {
-		super(...args)
+		super(...args) // superは親のメソッドを呼ぶ時につかう
 	}
 
 	print(label = '') {
 		console.log(`%c ${label}`, 'color: blue; font-weight: 600;', this);
 		return this;
+	}
+
+	push(val) {
+		super.push(val); // superは親のメソッドを呼ぶ時につかう
+		return this;
+	}
+
+	forEach(callback) {
+		for(let i = 0; i < this.length; i++) {
+			callback(this[i], i, this);
+		}
+	}
+
+	map(callback) {
+		const newInstance = new MyArray();
+		for(let i = 0; i < this.length; i++) {
+			const result = callback(this[i], i, this);
+			newInstance.push(result);
+		}
+		return newInstance;
+	}
+
+	filter(callback) {
+		const newInstance = new MyArray();
+		for (let i = 0; i < this.length; i++) {
+			if (callback(this[i], i, this)) {
+				newInstance.push(this[i]);
+			}
+		}
+		return newInstance;
+	}
+
+	reduce(callback, accu) {
+		const tmpArry = [...this];
+
+		if (accu === undefined) {
+			accu = tmpArry.shift();
+		}
+		for (let i = 0; i < tmpArry.length; i++) {
+			accu = callback(accu, this[i], this);
+			// newInstance.push(result);
+		}
+		return accu;
 	}
 }
 
@@ -27,16 +70,21 @@ function double(v, i, obj) {
 
 const original = new MyArray(1, 2, 3, 4);
 
+// original.push(6);
+
 const result = original
+	// .forEach(function (v, i, arry) {
+	// 	console.log(v, i, arry)
+	// })
 	.map(double)
 	.push(5)
 	.filter(function (v, i) {
 		return v > 2;
 	})
+	.print('place1')
 	.reduce(function(accu, curr) {
 		return accu + curr;
-	})
+	}, 0)
 
 console.log('%coriginal', 'color: blue; font-weight: bold;', original);
 console.log('%cresult', 'color: red; font-weight: bold;', result);
-
--- udemy/ガチで学びたい人のためのJavaScriptメカニズム/230310_100_様々なオブジェクト/920_修了確認_チェーンメソッド/start/main.js
+++ udemy/ガチで学びたい人のためのJavaScriptメカニズム/230310_100_様々なオブジェクト/920_修了確認_チェーンメソッド/start/main.js
@@ -3,16 +3,16 @@
  * 反復操作可能なオブジェクトIteratableObject
  * を作成してみましょう。
  * Arrayと同様に以下のメソッドが使用可能とします。
- * 
+ *
  * set(key, value)  *1
  * forEach
  * map
  * filter
- * 
+ *
  * *1:setはチェーンメソッドとしてつなげられるように実装してみてください。
- * 
+ *
  * また、for...ofを使用した際にはMapと同じ挙動を取るものとします。
- * 
+ *
  */
 class IteratableObject {
 	constructor(obj) {
@@ -26,6 +26,41 @@ class IteratableObject {
 		return this;
 	}
 
+	set(key, value) {
+		this[key] = value;
+		return this;
+	}
+
+	forEach(callback) {
+		for (let [k, v] of this) {
+			callback(v, k, this);
+		}
+	}
+
+	map(callback) {
+		const newInstance = new IteratableObject();
+		for (let [k, v] of this) {
+			newInstance[k] = callback(v, k, this);
+		}
+		return newInstance;
+	}
+
+	filter(callback) {
+		const newInstance = new IteratableObject();
+		for (let [k, v] of this) {
+			newInstance[k] = callback(v, k, this);
+			if (callback(v, k, this)) {
+				newInstance[k] = v;
+			}
+		}
+		return newInstance;
+	}
+
+	*[Symbol.iterator]() {
+		for (let key in this) {
+			yield [key, this[key]];
+		}
+	}
 }
 
 function prefix(v, i, obj) {
@@ -38,6 +73,14 @@ const original = new IteratableObject({
 	key3: 'value3',
 });
 
+original.forEach(v => {
+	console.log(v)
+})
+
+// for (let [k, v] of original) {
+// 	console.log(k, v);
+// }
+
 const result = original
 	.map(prefix)
 	.set('key4', 'value4')
@@ -49,9 +92,9 @@ console.log('%coriginal', 'color: blue; font-weight: bold;', original);
 console.log('%cresult', 'color: red; font-weight: bold;', result);
 /**
  * 期待する出力結果
- * original 
+ * original
  * {key1: "value1", key2: "value2", key3: "value3"}
- * 
- * result 
+ *
+ * result
  * {key4: "value4"}
  */
