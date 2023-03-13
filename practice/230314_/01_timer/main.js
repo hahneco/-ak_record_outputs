07b7c4653ecc9c86d014237c68372ae0441f6b9d
akiko_kuroda <hahnecochito@gmail.com>
2023-03-14 07:53:20 +0900

    タイマー作成

--- practice/230314_/01_timer/main.js
+++ practice/230314_/01_timer/main.js
@@ -1,9 +1,25 @@
 'use strict'
 
 function init() {
-  console.log('XXXXXXXXXXX');
+  console.log("init() 処理")
+  console.log('タイマー関連メソッド');
+}
 
+let timerID = NaN;
+let count = 0;
 
+function startTimer() {
+  timerID = setInterval(tick, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerID);
+  // console.log("stop")
+}
+
+function tick() {
+  count++;
+  document.getElementById("counter").textContent = count;
 }
 
 
