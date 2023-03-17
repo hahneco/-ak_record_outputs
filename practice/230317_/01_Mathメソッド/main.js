6c088fabbf6c21a3145d5e321763662cc38e0364
akiko_kuroda <hahnecochito@gmail.com>
2023-03-17 23:46:09 +0900

    Mathオブジェクトおさらい

--- practice/230317_/01_Mathメソッド/main.js
+++ practice/230317_/01_Mathメソッド/main.js
@@ -1,42 +1,10 @@
 'use strict'
 
-
-let startTime = 0;
 function update() {
-  // Q) ストップウォッチで経過時間を表示する
-
-  startTime = new Date();
-  // ①スタートボタンを押す→現在時刻を取得（表示）
-  // ②ストップボタンを押す→現在時刻を取得（表示）→「ストップ時刻-スタート時刻」を計算
-  // スタートtime
-  document.getElementById("hour").textContent = startTime.getHours();
-  document.getElementById("min").textContent = startTime.getMinutes();
-  document.getElementById("sec").textContent = startTime.getSeconds();
-  document.getElementById("msec").textContent = startTime.getMilliseconds();
+  console.log("update")
 }
 
-// 停止時間
-function stoptime() {
-  let stoptime = new Date();
-  // 停止時間
-  document.getElementById("stophour").textContent = stoptime.getHours();
-  document.getElementById("stopmin").textContent = stoptime.getMinutes();
-  document.getElementById("stopsec").textContent = stoptime.getSeconds();
-  document.getElementById("stopmsec").textContent = stoptime.getMilliseconds();
-
-  // 差分を計算
-  // let timeLag =
-  let hourLag = startTime.getHours() - stoptime.getHours();
-  let minLag = startTime.getMinutes() - stoptime.getMinutes();
-  let secLag = startTime.getSeconds() - stoptime.getSeconds();
-  let msecLag = startTime.getMilliseconds() - stoptime.getMilliseconds();
-
-  document.getElementById("hourlag").textContent = hourLag;
-  document.getElementById("minlag").textContent = minLag;
-  document.getElementById("seclag").textContent = secLag;
-  document.getElementById("mseclag").textContent = msecLag;
-}
-
-
-
-console.log("グローバルな文脈")
+function random() {
+    let r = Math.floor(Math.random() * 6); // Math.floor(n)でいっていの整数にマッピング（randoめメソッドで生成される数字が0〜1までの少数のため併せて使用する）
+    document.getElementById("value").textContent = r;
+  }
