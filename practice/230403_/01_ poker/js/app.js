0bae8a1959e8b5eab3c58dbf5ad5fe88678d4815
akiko_kuroda <hahnecochito@gmail.com>
2023-04-04 02:00:50 +0900

    Poker 見た目側作成

--- practice/230403_/01_ poker/js/app.js
+++ practice/230403_/01_ poker/js/app.js
@@ -1,3 +1,40 @@
 'use strict';
 
 // アプリケーションを起動する制御モジュール
+
+// 定数・広域変数
+const field = document.getElementById("field");
+
+onload = function () {
+  console.log("onload!");
+
+  tableSet("com");
+  tableSet("you");
+
+  field.style = "display: block; opacity: 1; transition-duration: 0.8s; transition: ease-in-out;";
+}
+
+// 初期画面カードをセット
+function tableSet(whose) {
+  const comTr = document.getElementById("comTr");
+  const youTr = document.getElementById("youTr");
+
+  for (let i = 0; i < 5; i++) { // td生成
+    let td = document.createElement("td");
+    let cardImg = document.createElement("img");
+
+    if (whose == "com") {
+      cardImg.src = 'images/' + 'red' + '.svg'; // comの場合
+      cardImg.alt = 'COMのカード_赤'; // comの場合
+      comTr.appendChild(td);
+    } else if (whose == "you") {
+      cardImg.src = 'images/' + 'blue' + '.svg'; // youの場合
+      cardImg.alt = 'youのカード_青'; // youの場合
+      youTr.appendChild(td);
+    }
+
+    td.className = "cell"
+    td.appendChild(cardImg);
+    cardImg.className = "card " + whose;
+  }
+}
