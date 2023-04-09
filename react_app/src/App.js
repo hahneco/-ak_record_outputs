a4d1dde136bf2c7b1d8244218fa04bdb02c0dc75
akiko_kuroda <hahnecochito@gmail.com>
2023-04-09 15:36:25 +0900

    react-app コンポーネントの作成〜export/import

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Rect from './Rect';
 import { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super()
+
+    this.title = props.title
+    this.message = props.message
+  }
+
   render() {
     return (
       <div>
         <h1 className='bg-blue text-white display-4'>React</h1>
         <div className='container'>
-          <p className='subtitle'>This is sample component.</p>
-          <p>これはsampleのコンポーネントです</p>
+          <p className='subtitle'>{this.title}</p>
+          <p>{this.message}</p>
           <p>簡単なmessageを表示します</p>
+
+          <hr></hr>
+          <Rect x="200" y="200" w="200" h="200" c="#F3F9FF" r="25" />
+          <Rect x="300" y="300" w="300" h="300" c="#DC9FB4" r="75" />
+          <Rect x="400" y="400" w="400" h="400" c="#A5DEE4" r="200" />
         </div>
       </div>
     )
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -12,6 +12,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <App title="App" message="This is App Component!" /> */}
     <App />
   </React.StrictMode>
 );
