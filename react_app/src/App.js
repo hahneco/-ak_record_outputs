fb9aa0214ed1e5b3a8a5813653f064f24db58727
akiko_kuroda <hahnecochito@gmail.com>
2023-04-10 02:13:53 +0900

    react-app 複数ステートの操作

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -5,8 +5,12 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [flag, setFlag] = useState(false)
   const clickFunk = () => {
     setCount(count + 1)
+  }
+  const changeFlag = (e) => {
+    setFlag(e.target.checked)
   }
 
   return (
@@ -14,11 +18,28 @@ function App() {
       <h1 className='bg-blue text-white display-4'>React</h1>
       <div className='container'>
         <h4 className='my-3'>Hooks sample</h4>
-        <div className='alert alert-primary text-center'>
-          <p className='h5'>click: {count} times!</p>
-          <div>
-            <button className='btn btn-primary' onClick={clickFunk}>Click me!</button>
+        {flag?
+          <div className='alert alert-primary text-center'>
+            <p className='h5'>click: {count} times!</p>
+            <div>
+              <button className='btn btn-primary' onClick={clickFunk}>Click me!</button>
+            </div>
           </div>
+        :
+          <div className='card p-3 border-primary text-center'>
+            <p className='h5 mb-3 text-left text-primary'>
+              click: {count} times!
+            </p>
+            <div>
+              <button className='btn btn-primary' onClick={changeFlag}>Click me</button>
+            </div>
+          </div>
+        }
+        <div className='form-group h6 text-container pt-3'>
+          <input type="checkbox" className='form-check-input' id='check1' onChange={changeFlag} />
+          <label className='form-check-label' htmlFor='check1'>
+            Change form style.
+          </label>
         </div>
       </div>
     </div>
