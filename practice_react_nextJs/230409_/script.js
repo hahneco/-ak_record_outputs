0838a587334d60db98b74d89bc890163cd44737c
akiko_kuroda <hahnecochito@gmail.com>
2023-04-09 14:06:26 +0900

    JSX　計算するコンポーネント

--- practice_react_nextJs/230409_/script.js
+++ practice_react_nextJs/230409_/script.js
@@ -8,12 +8,28 @@ function Welcome(props) {
   </div>
 }
 
+function Calc(props) {
+  let total = 0;
+  for (let i = 1; i < props.number; i++) {
+    total += 1;
+  }
+  return <p className={props.classes}>
+    1から{props.number}までの
+    合計は、「{total}」です。
+  </p>
+}
+
 // 表示するJSX
 let el = (
   <div>
     <h5 className="mb-4">{message}</h5>
-    <Welcome name="Taro" fontSize="h2" alert="alert alert-primary" />
-    <Welcome name="Hanako" fontSize="h5" alert="alert alert-dark" />
+    {/* <Welcome name="Taro" fontSize="h2" alert="alert alert-primary" />
+    <Welcome name="Hanako" fontSize="h5" alert="alert alert-dark" /> */}
+
+    <Welcome number="10" classes="h3" />
+    <Welcome number="100" classes="h5" />
+    <Calc number="50" classes="h4" />
+    <Calc number="500" classes="h6" />
   </div>
 )
 
