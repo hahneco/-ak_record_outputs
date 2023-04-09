2b302c1b7591d9ff93c4b68b6021b58a4d2bad2f
akiko_kuroda <hahnecochito@gmail.com>
2023-04-10 06:57:01 +0900

    react-app hook アルゴリズムをフックに抽出する

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,37 +4,65 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
-const useTax = (t1, t2) => {
-  const [price, setPrice] = useState(1000)
-  const [tx1] = useState(t1)
-  const [tx2] = useState(t2)
+const total = (a) => {
+  let re = 0
+  for (let i = 0; i <= a; i++) {
+    re += i
+  }
+  return re
+}
 
-  const tax = () => {
-    return Math.floor(price * (1.0 + tx1 / 100))
+const tax = (a) => {
+  return Math.floor(a * 1.1)
+}
+
+function useCalc(num = 0, func = (a) => { return a }) {
+  const [msg, setMsg] = useState(null)
+
+  const setValue = (p) => {
+    let res = func(p)
+    setMsg(<p className='h5'>※{p}の結果は、{res}です。</p>)
   }
 
-  const reduced = () => {
-    return Math.floor(price * (1.0 + tx2 / 100))
+  return [msg, setValue]
+}
+
+function PlainMessage(props) {
+  const [msg, setCalc] = useCalc()
+
+  const onChange = (e) => {
+    setCalc(e.target.value)
   }
 
-  return [price, tax, reduced, setPrice]
+  return <div className='p-3 h5'>
+    <h5>{msg}</h5>
+    <input type="number" onChange={onChange} className='form-control' />
+  </div>
 }
 
 function AlertMessage(props) {
-  const [price, tax, reduced, setPrice] = useTax(10, 8)
+  const [msg, setCalc] = useCalc(0, total)
 
-  const DoChange = (e) => {
-    let p = e.target.value
-    setPrice(p)
+  const onChange = (e) => {
+    setCalc(e.target.value)
   }
 
   return <div className='alert alert-primary h5 text-center'>
-    <p className='h5'>通常税率: {tax()} 円.</p>
-    <p className='h5'>軽減税率: {reduced()} 円.</p>
-    <div className='form-group'>
-      <label className='form-group-label'>Price:</label>
-      <input type="text" className='form-control' onChange={DoChange} value={price} />
-    </div>
+    <p className='h5'>{msg}</p>
+    <input type="number" onChange={onChange} min="0" max="10000" className='form-control' />
+  </div>
+}
+
+function CardMessage(props) {
+  const [msg, setCalc] = useCalc(0, tax)
+
+  const onChange = (e) => {
+    setCalc(e.target.value)
+  }
+
+  return <div className='card p-3 h5 border-primary'>
+    <h5>{msg}</h5>
+    <input type="range" onChange={onChange} min="0" max="10000" step="100" className='form-control' />
   </div>
 }
 
@@ -45,7 +73,9 @@ function App() {
       <h1 className='bg-blue text-white display-4'>React</h1>
       <div className='container'>
         <h4 className='my-3'>Hooks sample</h4>
+        <PlainMessage />
         <AlertMessage />
+        <CardMessage />
       </div>
     </div>
   )
