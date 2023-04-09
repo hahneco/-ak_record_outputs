a8fd258092f25dc787f011016e8b64fa09529218
akiko_kuroda <hahnecochito@gmail.com>
2023-04-09 14:11:21 +0900

    JSX コンポーネントのクラス

--- practice_react_nextJs/230409_/script.js
+++ practice_react_nextJs/230409_/script.js
@@ -1,35 +1,22 @@
 let dom = document.querySelector("#root");
 let message = "React component page."
 
-// 関数コンポーネント
-function Welcome(props) {
-  return <div className={ props.alert }>
-    <p className={ props.fontSize }>Hello { props.name }</p>
-  </div>
-}
-
-function Calc(props) {
-  let total = 0;
-  for (let i = 1; i < props.number; i++) {
-    total += 1;
+class Hello extends React.Component {
+  constructor(props) {
+    super(props)
   }
-  return <p className={props.classes}>
-    1から{props.number}までの
-    合計は、「{total}」です。
-  </p>
+
+  render() {
+    return <div className="alert alert-primary">
+      <p className="h4">Hello〜!!!!</p>
+    </div>
+  }
 }
 
-// 表示するJSX
 let el = (
   <div>
     <h5 className="mb-4">{message}</h5>
-    {/* <Welcome name="Taro" fontSize="h2" alert="alert alert-primary" />
-    <Welcome name="Hanako" fontSize="h5" alert="alert alert-dark" /> */}
-
-    <Welcome number="10" classes="h3" />
-    <Welcome number="100" classes="h5" />
-    <Calc number="50" classes="h4" />
-    <Calc number="500" classes="h6" />
+    <Hello />
   </div>
 )
 
