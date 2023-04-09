b52592b9ca916af1ef8ae09b03c868e2bbd4ccbf
akiko_kuroda <hahnecochito@gmail.com>
2023-04-09 18:51:51 +0900

    react-app 内部のテキストをリスト表示する

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,71 +4,50 @@ import Rect from './Rect';
 import { Component } from 'react';
 
 class App extends Component {
-  data = [
-    "This is list sample",
-    "これはリストのサンプル!",
-    "配列をリストへ変換する!"
-  ]
+  input = ''
 
   constructor(props) {
     super(props)
-
-    this.state = {
-      list: this.data
-    }
   }
+
   render() {
     return <div>
       <h1 className='bg-blue text-white display-4'>React</h1>
       <div className='container'>
-        <p className='subtitle'>show list.</p>
-        <List title="サンプル・リスト" data={this.data} />
+        <Message title="Children!">
+          これはコンポーネント内のコンテンツです。
+          マルでテキストを分割し、listで表示します。
+          改行は必要ありません。
+        </Message>
       </div>
     </div>
   }
 }
 
-class List extends Component {
-  number = 1
-
-  render() {
-    let data = this.props.data;
-    return (
-      <div>
-        <p className='h5 text-center'>{this.props.title}</p>
-        <ul className='list-group'>
-          {data.map((item, key) =>
-            <li className='list-group-item' key={key}>
-              <Item number={key + 1} value={item} />
-            </li>
-          )}
-        </ul>
-      </div>
-    )
-  }
-}
-
-class Item extends Component {
-  item = {
-    fontSize: "16px",
-    color: "#00f",
-    textDecoration: "underline",
-    textDecorationColor: "#ddf"
-  }
-
-  num = {
+class Message extends Component {
+  li = {
+    fontSize: "14pt",
     fontWeight: "bold",
     color: "#A5DEE4"
   }
 
   render() {
+    let content = this.props.children
+    let arr = content.split('。')
+    let arr2 = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].trim() != '') {
+        arr2.push(arr[i]);
+      }
+    }
+    let list = arr2.map((value, key) => (
+      <li className='list-group-item' style={this.li} key={key}>{key + 1}. {value} .</li>
+    ))
     return (
-      <p style={this.item}>
-        <span style={this.num}>
-          [{this.props.number}]&nbsp;
-        </span>
-        {this.props.value}
-      </p>
+      <div>
+        <h2>{this.props.title}</h2>
+        <ol className='list-group'>{list}</ol>
+      </div>
     )
   }
 }
