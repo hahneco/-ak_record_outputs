9e7921e7708af1701937af854f6dd7c813419b14
akiko_kuroda <hahnecochito@gmail.com>
2023-04-09 12:46:00 +0900

    JSX アロー関数、mapメソッド

--- practice_react_nextJs/230409_/script.js
+++ practice_react_nextJs/230409_/script.js
@@ -1,45 +1,73 @@
-const msg_s = {
-  fontSize: "20pt",
-  fontWeight: "bold",
-  color: "green",
-  border: "1px dotted green"
-}
-const msg = {
-  fontSize: "20pt",
-  fontWeight: "bold",
-  color: "green",
-  border: "1px dotted green"
-}
+let dom = document.querySelector("#root");
 
-let dom = document.querySelector('#root');
-let title = "React Page";
-let message = "メッセージを表示します";
-let link = "http://google.com";
+let title = "React page."
+let message = "メッセージを表示します"
+let content = '※これが、trueの時に表示されるメッセージ!..表示できてる?;'
+let content_true = '※これが、trueの時に表示されるメッセージ!..表示できてる?;'
+let content_false = '※これが、falseの時に表示されるメッセージ!..表示できてる?;'
 
-let printMsg = function(msg, size, color) {
-  const style = {
-    fontSize: size + "pt",
-    fontWeight: '700',
-    color: color,
-    border: "1px solid" + color
-  }
-  return <p style={style}>{msg}</p>
-}
+let flg = false; // ☆
+
+let data = [
+  <li className="list-group-item">One</li>,
+  <li className="list-group-item">Two</li>,
+  <li className="list-group-item">Three</li>
+]
+
+let data2 = [
+  {name: 'Taro', mail:'@taro@google.com', age: 45},
+  {name: 'Rika', mail:'@rika@google.com', age: 37},
+  {name: 'Hanako', mail:'@hanako@google.com', age: 49},
+  {name: 'Takeshi', mail:'@takashi@google.com', age: 40},
+  {name: 'Jiro', mail:'@jiro@google.com', age: 25},
+  {name: 'Keiko', mail:'@keiko@google.com', age: 15}
+]
 
 let el = (
   <div>
-    <h2>{title}</h2>
-    <p>renderできるのは１つのエレメントのみ。この場合はdivで括って生成するエレメントをまとめる</p>
-    <hr></hr>
-    <h3><a href={link}>属性をくくる「"」はいらない</a></h3>
-    <p style={msg_s}>sampleでーす</p>
-    <h3>{title}</h3>
-    <h3>{message}</h3>
-    <div className="alert alert-secondary mt-3">
-      {printMsg('最初のメッセージ', 36, '#fff')}
-      {printMsg('次のメッセージ', 36, '#aaa')}
-      {printMsg('最後のメッセージ', 36, '#666')}
-    </div>
+    <h4>{title}</h4>
+    <h6>{message}</h6>
+    {
+      flg &&
+      <div className="alert alert-primary mt-3">
+          <p>{content}</p>
+      </div>
+    }
+    {
+      flg ?
+      <div className="alert alert-primary mt-3">
+          <p>3項演算子ver: true</p>
+          <p>{content_true}</p>
+      </div>
+      :
+      <div className="alert alert-primary mt-3">
+        <p>3項演算子ver: false</p>
+        <p>{content_false}</p>
+      </div>
+    }
+    <ul className="list-group mt-4">
+      {data}
+    </ul>
+    <table className="container table table-striped mt-4">
+      <thead className="thead-dark">
+        <tr>
+          <th>name</th>
+          <th>mail</th>
+          <th>age</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data2.map((value) => (
+            <tr>
+              <td>{value.name}</td>
+              <td>{value.name}</td>
+              <td>{value.name}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   </div>
 )
 
