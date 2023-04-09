ae60cf996f84f0c60fbc5369b4f767d38d3bd364
akiko_kuroda <hahnecochito@gmail.com>
2023-04-09 12:22:45 +0900

    JSX sprict.jsに移植

--- practice_react_nextJs/230409_/script.js
+++ practice_react_nextJs/230409_/script.js
@@ -1,60 +1,46 @@
-'use strict';
-
-let counter = 0;
-let dom = document.querySelector('#root');
-doCount();
-
-function doCount() {
-  counter++;
-  let element = React.createElement(
-    // 'p', "count", counter
-
-    // 仮想DOMへのエレメント追加
-    // React.createElement(タグ名, 属性)
-
-     // 仮想DOMへのエレメント追加(入れ子)
-    // React.createElement(タグ名, 属性, React.createElement(タグ名, 属性, React.createElement(タグ名, 属性,)))
-    'div', {}, [
-      React.createElement(
-        'h2', {}, "Hello!"
-      ),
-      React.createElement(
-        'h4', { className: 'alert alert-primary' },
-        "React sample page!"
-      ),
-      React.createElement(
-        'ul', {className: 'list-group'}, [
-          React.createElement(
-            'li', {className: 'list-group-item'}, "First item"
-          ),
-          React.createElement(
-            'li', {className: 'list-group-item'}, "Second item"
-          ),
-          React.createElement(
-            'li', {className: 'list-group-item'}, "Third item."
-          )
-        ]
-      ),
-
-      React.createElement(
-        'div', { className: 'container mt-3' }, [
-          React.createElement(
-            'div', {className: 'bg-primary text-white p-2'}, "primary"
-          ),
-          React.createElement(
-            'div', {className: 'bg-secondary text-white p-2'}, "secondary"
-          ),
-          React.createElement(
-            'div', {className: 'bg-success text-white p-2'}, "success"
-          ),
-          React.createElement(
-            'div', {className: 'bg-warning text-white p-2'}, "warning"
-          ),
-        ]
-      )
-    ]
-  )
-  // rendering: プログラムのオブジェクトを具体的に目に見える形に変換する作業
-  // 表示を更新したければ、エレメントを作ってレンダリングし直す。
-  ReactDOM.render(element, dom);
+const msg_s = {
+  fontSize: "20pt",
+  fontWeight: "bold",
+  color: "green",
+  border: "1px dotted green"
 }
+const msg = {
+  fontSize: "20pt",
+  fontWeight: "bold",
+  color: "green",
+  border: "1px dotted green"
+}
+
+let dom = document.querySelector('#root');
+let title = "React Page";
+let message = "メッセージを表示します";
+let link = "http://google.com";
+
+let printMsg = function(msg, size, color) {
+  const style = {
+    fontSize: size + "pt",
+    fontWeight: '700',
+    color: color,
+    border: "1px solid" + color
+  }
+  return <p style={style}>{msg}</p>
+}
+
+let el = (
+  <div>
+    <h2>{title}</h2>
+    <p>renderできるのは１つのエレメントのみ。この場合はdivで括って生成するエレメントをまとめる</p>
+    <hr></hr>
+    <h3><a href={link}>属性をくくる「"」はいらない</a></h3>
+    <p style={msg_s}>sampleでーす</p>
+    <h3>{title}</h3>
+    <h3>{message}</h3>
+    <div className="alert alert-secondary mt-3">
+      {printMsg('最初のメッセージ', 36, '#fff')}
+      {printMsg('次のメッセージ', 36, '#aaa')}
+      {printMsg('最後のメッセージ', 36, '#666')}
+    </div>
+  </div>
+)
+
+ReactDOM.render(el, dom);
