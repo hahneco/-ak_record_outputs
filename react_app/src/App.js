b4be5ab0f304967e7e1c932cbace71480851ff20
akiko_kuroda <hahnecochito@gmail.com>
2023-04-09 19:37:12 +0900

    react-app フォームの利用

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -8,17 +8,41 @@ class App extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      title: 'input form',
+      message: 'type your name.'
+    }
+    this.doChange = this.doChange.bind(this)
+    this.doSubmit = this.doSubmit.bind(this)
+  }
+
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
+    this.setState({
+      title: 'send form',
+      message: 'hello, ' + this.input + '!'
+    })
+    event.preventDefault()
   }
 
   render() {
     return <div>
       <h1 className='bg-blue text-white display-4'>React</h1>
       <div className='container'>
-        <Message title="Children!">
-          これはコンポーネント内のコンテンツです。
-          マルでテキストを分割し、listで表示します。
-          改行は必要ありません。
-        </Message>
+        <h4>{this.state.title}</h4>
+        <p className='card h5 p-3'>{this.state.message}</p>
+        <div className='alert alert-primary mt-3'>
+          <form onSubmit={this.doSubmit}>
+            <div className='form-group'>
+              <label>Message:</label>
+              <input type="text" className='form-control' onChange={this.doChange} required pattern='[A-Za-z _,.]+' />
+            </div>
+            <input type="submit" className='btn btn-primary' value="Click" />
+          </form>
+        </div>
       </div>
     </div>
   }
