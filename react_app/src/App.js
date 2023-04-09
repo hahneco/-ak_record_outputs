5b23b93d85760b38a26ea4faa51bcbdf6852618f
akiko_kuroda <hahnecochito@gmail.com>
2023-04-09 20:16:32 +0900

    react-app コンテキスト

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,61 +1,49 @@
 // import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Rect from './Rect';
-import { Component } from 'react';
+// import Rect from './Rect';
+
+
+let data = {title: 'React-Context',
+  message: 'this is sample message'
+}
+
+const SampleContext = React.createContext(data)
 
 class App extends Component {
-  input = ''
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: 'input form',
-      message: 'type your name.',
-      max: 10 // ☆
-    }
-    this.doCheck = this.doCheck.bind(this)
-  }
-
-  doCheck(event) {
-    alert(event.target.value + "は長すぎます。(最大" + this.state.max + "文字)")
-  }
-
   render() {
     return <div>
       <h1 className='bg-blue text-white display-4'>React</h1>
       <div className='container'>
-        <h4>{this.state.title}</h4>
-        <Message maxlength={this.state.max} onCheck={this.doCheck} />
+        <Title />
+        <Message />
       </div>
     </div>
   }
 }
 
-class Message extends Component {
-  li = {
-    fontSize: "14pt",
-    fontWeight: "bold",
-    color: "#A5DEE4"
-  }
-
-  constructor(props) {
-    super(props)
-    this.doChange = this.doChange.bind(this)
-  }
-
-  doChange(e) {
-    if (e.target.value.length > this.props.maxlength) {
-      this.props.onCheck(e)
-      e.target.value = e.target.value.substr(0, this.props.maxlength)
-    }
-  }
+class Title extends Component {
+  static contextType = SampleContext
 
   render() {
-    return <div className='form-group'>
-      <label>input:</label>
-      <input type="text" className='form-control' onChange={this.doChange} />
-    </div>
+    return (
+      <div className='card p-2 my-3'>
+        <h2>{this.context.title}</h2>
+      </div>
+    )
   }
 }
 
-export default App;
+class Message extends Component {
+  static contextType = SampleContext
+
+  render() {
+    return (
+      <div className='alert alert-primary'>
+        <p>{this.context.message}</p>
+      </div>
+    )
+  }
+}
+
+export default App
