54fe77ed2826392ddfdd847dd6d1226276c3630a
akiko_kuroda <hahnecochito@gmail.com>
2023-04-11 08:25:36 +0900

    reversible_piece　途中

--- practice_react_nextJs/reversible-piece-game/src/App.js
+++ practice_react_nextJs/reversible-piece-game/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import GamePage from './game/GamePage' // ゲーム全体を１つにまとめるコンポーネント
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='bg-blue text-white display-4'>React</h1>
+      <div className='container'>
+        <h2 className='my-3'>Reversible Piece<span>!</span></h2>
+        <GamePage />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
