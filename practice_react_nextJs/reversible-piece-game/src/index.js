4477495281fa1a3adc2e7b3077f8100ee760545f
akiko_kuroda <hahnecochito@gmail.com>
2023-04-20 02:54:00 +0900

    react　オセロ　途中

--- practice_react_nextJs/reversible-piece-game/src/index.js
+++ practice_react_nextJs/reversible-piece-game/src/index.js
@@ -24,7 +24,7 @@ const WeightData = [ // 盤面ごとの優先度
 const BLACK = 1; // 自分
 const WHITE = 2; // PC
 // let data = []; // 盤データ(0:なし、1:黒、2:白) // squaresで代用
-let myTurn = false; // 自分ば番かどうか
+let xIsNext = false; // 自分ば番かどうか
 
 
 function Square(props) { // 自分のstateを持っていないので関数コンポーネントで定義
@@ -157,19 +157,19 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    // console.log(this.state.history[0].squares);
-    console.log(i);
-
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
     const line = i.slice(0, 1);
     const column = i.slice(1, 2);
+    // console.log(this.state.history[0].squares);
+    // console.log(i);
+
 
     // console.log(line)
     // console.log(column)
-    console.log(squares)
+    // console.log(squares)
 
     if (calculateWinner(squares) || squares[line][column]) {
       return;
@@ -182,6 +182,11 @@ class Game extends React.Component {
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
+
+    let blackFlip = this.canFlip(BLACK); // 黒反転できるか否か
+    let whiteFlip = this.canFlip(WHITE); // 白反転できるか否か
+
+    this.update();
   }
 
   jumpTo(step) {
@@ -189,6 +194,269 @@ class Game extends React.Component {
       stepNumber: step,
       xIsNext: (step % 2) === 0,
     });
+  }
+
+  update() { // 白/黒の数を数えて表示する
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    let numWhite = 0;
+    let numBlack = 0;
+    console.log(current.squares)
+    // console.log("current.squares[3][3]は、" + current.squares[3][3])
+    // console.log("current.squares[3][4]は、" + current.squares[3][4])
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        if (current.squares[x][y] == WHITE) {
+          numWhite++;
+          // console.log("numWhiteは、" + numWhite)
+        }
+        if (current.squares[x][y] == BLACK) {
+          numBlack++;
+          // console.log("numBlackは、" + numBlack)
+        }
+      }
+    }
+    // document.getElementById("numBlack").textContent = numBlack;
+    // document.getElementById("numWhite").textContent = numWhite;
+
+    let blackFlip = this.canFlip(BLACK); // 黒反転できるか否か
+    let whiteFlip = this.canFlip(WHITE); // 白反転できるか否か
+    const finish = numBlack + numWhite;
+
+    console.log(finish)
+    if (finish === 64 || (!blackFlip && !whiteFlip)) { // 64手すべて打ち終わったときに
+      console.log(blackFlip)
+      console.log(whiteFlip)
+      console.log("finish")
+      if (numWhite > numBlack) {
+        document.getElementById("message").textContent = "白の勝ち";
+      } else if (numWhite < numBlack) {
+        document.getElementById("message").textContent = "黒の勝ち";
+      } else {
+        document.getElementById("message").textContent = "引き分け";
+      }
+      return
+    }
+
+    //　石を置く順番を判定する処理
+    if (!blackFlip) {
+      this.showMessage("黒スキップ");
+      xIsNext = false;
+    } else if (!whiteFlip) {
+      this.showMessage("白スキップ");
+      xIsNext = true;
+    } else {
+      // 順番交代。xIsNextが true → false → true → false になる。
+      xIsNext = !xIsNext;
+    }
+    if (!xIsNext) {
+      // setTimeout(this.think, 1000); // 1秒間考える時間
+      this.think()
+      // this.hello("よっ!!")
+      // setTimeout(this.hello("やあ"), 2000); // 1秒間考える時間
+    }
+  }
+
+  hello(greeting) {
+    console.log(greeting);
+  }
+
+  // showMessage関数
+  showMessage(str) { // メッセージを2秒間表示
+    document.getElementById("message").textContent = str;
+    setTimeout(function () {
+      document.getElementById("message").textContent = "";
+    }, 2000); // メッセージ(str)を２秒間表示する
+  }
+
+  clicked(e) { // 盤上のセルclick時のコールバック関数
+    if (!xIsNext) {
+      return;
+    }
+    let id = e.target.id;
+    let i = parseInt(id.charAt(4)); // 文字列を整数に変換 縦の座標(i列)取得
+    let j = parseInt(id.charAt(5)); // 文字列を整数に変換 横の座標(j列)取得
+
+    let flipped = this.getFlipCells(i, j, BLACK); // 黒に反転する配列
+    if (flipped.length > 0) {
+      // console.log(flipped);
+      for (let k = 0; k < flipped.length; k++) {
+        this.put(flipped[k][0], flipped[k][1], BLACK); // 反転する石が0より多ければ黒に変換する
+      }
+      this.put(i, j, BLACK); // clickしたマスに黒を置く
+      this.update();
+    }
+  }
+
+  // put関数(i,j)にcolor色の石を置く
+  put(i, j, color) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    // let c = document.getElementById("cell" + i + j);
+    let c = squares[i][j];
+    console.log(c)
+    c.value = BLACK;
+    c.className = "cell " + (color === BLACK ? "black" : "white");
+    squares[i][j] = color;
+  }
+
+  // think関数(コンピューター思考関数)
+  think() {
+    let highScore = -1000;
+    let px = -1;
+    let py = -1;
+    let tmpData = this.copyData();
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        let flipped = this.getFlipCells(x, y, WHITE);
+        if (flipped.length > 0) {
+          for (let i = 0; i < flipped.length; i++) {
+            let p = flipped[i][0];
+            let q = flipped[i][1];
+            tmpData[p][q] = WHITE;
+            tmpData[x][y] = WHITE;
+          }
+          let score = this.calcWeightData(tmpData);
+          if (score > highScore) {
+            highScore = score;
+            // (px = x), (py = y);
+            (px = x);
+            (py = y);
+          }
+        }
+      }
+    }
+    if (px >= 0 && py >= 0) {
+      let flipped = this.getFlipCells(px, py, WHITE);
+      if (flipped.length > 0) {
+        for (let k = 0; k < flipped.length; k++) {
+          this.put(flipped[k][0], flipped[k][1], WHITE);
+        }
+      }
+      this.put(px, py, WHITE);
+    }
+
+    // this.update();
+  }
+
+  calcWeightData(tmpData) {
+    let score = 0;
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        if (tmpData[x][y] == WHITE) {
+          score += WeightData[x][y];
+        }
+      }
+    }
+    return score;
+  }
+
+  copyData() {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    let tmpData = [];
+    for (let x = 0; x < 8; x++) {
+      tmpData[x] = [];
+      for (let y = 0; y < 8; y++) {
+        tmpData[x][y] = squares[x][y];
+      }
+    }
+    return tmpData;
+  }
+
+  // canFlip関数:盤面に引数の色の石を置けるかをbooleanで返す。
+  canFlip(color) { // 挟める石があるか？
+    console.log("colorは"+color);
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        // 座標(x,y)に石を置いた時に反転する数を求める
+        let flipped = this.getFlipCells(x, y, color);
+        console.log(x)
+        console.log(y)
+        if (flipped.length > 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+
+  // getFlipCellか関数:(i,j)座標にcolorの石を置いた時に反転する石の配列を返す。
+  // 各方向を配列dirsに格納。for文で方向ごとに石を挟めるか判定する。
+  getFlipCells(i, j, color) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    // const line = i.slice(0, 1);
+    // const column = i.slice(1, 2);
+
+    // console.log(data) // 盤データ(0:なし、1:黒、2:白)
+    if (squares[i][j] === BLACK || squares[i][j] === WHITE) { // すでに石がある場合(配列す数字が1または2の時)関数から抜ける
+      // console.log(data)
+      return [];
+    }
+
+    // 相手を挟めるか調査
+    let dirs = [ // clickした座標を拠点に8方位を調査する
+      [-1, -1], // 左上
+      [0, -1], // 真上
+      [1, -1], // 右上
+      [-1, 0], // 左
+      [1, 0], // 右
+      [-1, 1], // 左下
+      [0, 1], // 真下
+      [1, 1], // 右下
+    ];
+    let result = [];
+    for (let p = 0; p < dirs.length; p++) {
+      let flipped = this.getFlipCellsOneDir(i, j, dirs[p][0], dirs[p][1], color);
+      result = result.concat(flipped); // 別の配列と結合する処理
+    }
+    return result;
+  }
+
+  // (i,j)を起点として、(dx,dy)方向に、color色の石で挟めるかを返す。
+  getFlipCellsOneDir(i, j, dx, dy, color) { // (i,j)に石を置いた時に、(dx,dy)方向で石を挟めるか？
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    let x = i + dx; // dx方向に順番に見ていくときの座標
+    let y = j + dy; // dy方向に順番に見ていくときの座標
+    let flipped = []; // 挟まれた石の配列
+    // console.log(i + "," + j)
+
+    if (
+      x < 0 || // 盤の外(その行の左にマス目がない)
+      y < 0 || // 盤の外(その行の上にマス目がない)
+      x > 7 || // 盤の外(その行の右にマス目がない)
+      y > 7 || // 盤の外(その行の下にマス目がない)
+      squares[i][j] == color || // 同じ色
+      squares[i][j] == 0 // 石がない
+    ) {
+      return []; // 盤外、同色、空ならfalse(挟めない)ので関数を抜ける
+    }
+    flipped.push([x, y]);
+    while (true) {
+      x += dx;
+      y += dy;
+      if (x < 0 || y < 0 || x > 7 || y > 7 || squares[i][j] == 0) {
+        return []; // 盤外、空ならfalse(挟めない)
+      }
+      if (squares[i][j] == color) {
+        return flipped;
+      } else {
+        flipped.push([x, y]);
+      }
+    }
   }
 
   render() {
@@ -229,27 +497,6 @@ class Game extends React.Component {
       }
     }
 
-    let blackFlip = canFlip(BLACK); // 黒反転できるか否か
-    let whiteFlip = canFlip(WHITE); // 白反転できるか否か
-
-    // canFlip関数:盤面に引数の色の石を置けるかをブーリアンで返す。
-    canFlip(i) { // 挟める石があるか？
-      for (let x = 0; x < 8; x++) {
-        for (let y = 0; y < 8; y++) {
-          // 座標(x,y)に石を置いた時に反転する数を求める
-          let flipped = getFlipCells(x, y, i);
-          // console.log(x)
-          // console.log(y)
-          if (flipped.length > 0) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-
-    getFlipCells()
-
 
     return (
       <div className="game">
@@ -260,7 +507,7 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <div>メッセージ:&nbsp;{status}</div>
+          <div id={"message"}>メッセージ:&nbsp;{status}</div>
           <div>黒（あなた）:&nbsp;{numBlack}枚</div>
           <div>白（PC）:&nbsp;{numWhite}枚</div>
           <ol>{moves}</ol>
