1939072d85c6c8782c2188e832d417eb5c172e6e
akiko_kuroda <hahnecochito@gmail.com>
2023-04-25 07:55:29 +0900

    react 関数コンポーネント化　途中

--- practice_react_nextJs/reversible-piece-game/src/game/Com.js
+++ practice_react_nextJs/reversible-piece-game/src/game/Com.js
@@ -1,88 +1,84 @@
+import React, { useState, Component } from 'react';
 import { DATA, PIECE } from "./constants.js";
-import Game from "./Game.js";
+// import Game from "./Game.js";
+import {update} from './Update.js';
 
 // Com(相手) クラス
 
-export default class Com extends Game {
-  constructor(props) {
-    super(props)
-  }
+export async function useThink(props) {
+  const [history, setHistory] = useState(this.state.history.slice(0, this.state.stepNumber + 1))
+  const [current] = useState(history[history.length - 1])
+  const [squares, setSquare] = useState(current.squares.slice())
 
-  think() {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-
-    let highScore = -1000;
-    let px = -1;
-    let py = -1;
-    let tmpData = this.copyData();
-    for (let x = 0; x < 8; x++) {
-      for (let y = 0; y < 8; y++) {
-        let flipped = this.getFlipCells(x, y, PIECE.white);
-        if (flipped.length > 0) {
-          for (let i = 0; i < flipped.length; i++) {
-            let p = flipped[i][0];
-            let q = flipped[i][1];
-            tmpData[p][q] = PIECE.white;
-            tmpData[x][y] = PIECE.white;
-          }
-          let score = this.calcWeightData(tmpData);
-          if (score > highScore) {
-            highScore = score;
-            (px = x);
-            (py = y);
-          }
-        }
-      }
-    }
-    if (px >= 0 && py >= 0) {
-      let flipped = this.getFlipCells(px, py, PIECE.white);
+  let highScore = -1000;
+  let px = -1;
+  let py = -1;
+  let tmpData = this.copyData();
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      let flipped = this.getFlipCells(x, y, PIECE.white);
       if (flipped.length > 0) {
-        for (let k = 0; k < flipped.length; k++) {
-          this.put(flipped[k][0], flipped[k][1], PIECE.white);
+        for (let i = 0; i < flipped.length; i++) {
+          let p = flipped[i][0];
+          let q = flipped[i][1];
+          tmpData[p][q] = PIECE.white;
+          tmpData[x][y] = PIECE.white;
         }
-      }
-      this.put(px, py, PIECE.white);
-    }
-
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      // gameSystem.isMyTurn: !this.state.gameSystem.isMyTurn,
-    });
-
-    this.update();
-  }
-
-  calcWeightData(tmpData) {
-    let score = 0;
-    for (let x = 0; x < 8; x++) {
-      for (let y = 0; y < 8; y++) {
-        if (tmpData[x][y] === PIECE.white) {
-          score += DATA.weightData[x][y];
+        let score = this.calcWeightData(tmpData);
+        if (score > highScore) {
+          highScore = score;
+          (px = x);
+          (py = y);
         }
       }
     }
-    return score;
   }
-
-  copyData() {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-
-    let tmpData = [];
-    for (let x = 0; x < 8; x++) {
-      tmpData[x] = [];
-      for (let y = 0; y < 8; y++) {
-        tmpData[x][y] = squares[x][y];
+  if (px >= 0 && py >= 0) {
+    let flipped = this.getFlipCells(px, py, PIECE.white);
+    if (flipped.length > 0) {
+      for (let k = 0; k < flipped.length; k++) {
+        this.put(flipped[k][0], flipped[k][1], PIECE.white);
       }
     }
-    return tmpData;
+    this.put(px, py, PIECE.white);
   }
+
+  this.setState({
+    history: history.concat([{
+      squares: squares,
+    }]),
+    stepNumber: history.length,
+    // gameSystem.isMyTurn: !this.state.gameSystem.isMyTurn,
+  });
+
+  update();
+}
+
+export async function calcWeightData(tmpData) {
+  let score = 0;
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      if (tmpData[x][y] === PIECE.white) {
+        score += DATA.weightData[x][y];
+      }
+    }
+  }
+  return score;
+}
+
+export async function copyData() {
+  const history = this.state.history.slice(0, this.state.stepNumber + 1);
+  const current = history[history.length - 1];
+  const squares = current.squares.slice();
+
+  let tmpData = [];
+  for (let x = 0; x < 8; x++) {
+    tmpData[x] = [];
+    for (let y = 0; y < 8; y++) {
+      tmpData[x][y] = squares[x][y];
+    }
+  }
+  return tmpData;
 }
 
 // think関数(コンピューター思考関数)
--- practice_react_nextJs/reversible-piece-game/src/game/Game.js
+++ practice_react_nextJs/reversible-piece-game/src/game/Game.js
@@ -1,7 +1,8 @@
 import React, { useState, Component } from 'react';
 import { PIECE, gameSystem } from "./constants.js";
 import Util from "./Util.js";
-import Com from "./Com.js"
+import {useThink} from "./Com.js"
+// import {update} from './Update.js';
 import Board from '../index.js';
 
 export default class Game extends React.Component {
@@ -26,90 +27,91 @@ export default class Game extends React.Component {
   }
 
   async update() { // 白/黒の数を数えて表示する
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
+  const history = this.state.history.slice(0, this.state.stepNumber + 1);
+  const current = history[history.length - 1];
+  const squares = current.squares.slice();
 
-    let numWhite = 0; // あとでBoardに持たせる
-    let numBlack = 0; // あとでBoardに持たせる
+  let numWhite = 0; // あとでBoardに持たせる
+  let numBlack = 0; // あとでBoardに持たせる
 
-    console.log("this.state.stepNumber" + this.state.stepNumber)
-    console.log("this.state.history[0].squares" + this.state.history[0].squares)
-    console.log(current.squares)
+  console.log("this.state.stepNumber" + this.state.stepNumber)
+  console.log("this.state.history[0].squares" + this.state.history[0].squares)
+  console.log(current.squares)
 
-    for (let x = 0; x < 8; x++) {
-      for (let y = 0; y < 8; y++) {
-        if (squares[x][y] === PIECE.white) {
-          numWhite++;
-        }
-        if (squares[x][y] === PIECE.black) {
-          numBlack++;
-        }
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      if (squares[x][y] === PIECE.white) {
+        numWhite++;
       }
-    }
-    console.log("numWhiteは、" + numWhite + "枚")
-    console.log("numBlackは、" + numBlack + "枚")
-
-    let blackFlip = this.canFlip(PIECE.black); // 黒反転できるか否か // canFlip関数:盤面に引数の色の石を置けるかをbooleanで返す。
-    let whiteFlip = this.canFlip(PIECE.white); // 白反転できるか否か // canFlip関数:盤面に引数の色の石を置けるかをbooleanで返す。
-    const sumWhiteandBlack = numBlack + numWhite;
-
-    console.log("blackFlip: " + blackFlip)
-    console.log("whiteFlip: " + whiteFlip)
-    console.log("sumWhiteandBlack: " + sumWhiteandBlack)
-
-    if (sumWhiteandBlack === 64 || (!blackFlip && !whiteFlip)) { // 64手すべて打ち終わったときに
-      console.log("finish")
-      if (numWhite > numBlack) {
-        this.setState({
-          situation: "白の勝ち"
-        })
-      } else if (numWhite < numBlack) {
-        this.setState({
-          situation: "黒の勝ち"
-        })
-      } else {
-        this.setState({
-          situation: "引き分け"
-        })
+      if (squares[x][y] === PIECE.black) {
+        numBlack++;
       }
-      return
-    }
-
-    console.log("gameSystem.isMyTurnは、" + gameSystem.isMyTurn)
-
-    // 石を置く順番を判定する処理
-    if (!blackFlip) {
-      this.showMessage("黒スキップ");
-      gameSystem.isMyTurn = false;
-      // this.setState({ // 変更したstateをセットする
-      //   gameSystem.isMyTurn: false,
-      // });
-
-    } else if (!whiteFlip) {
-      this.showMessage("白スキップ");
-      gameSystem.isMyTurn = true;
-      // this.setState({ // 変更したstateをセットする
-      //   gameSystem.isMyTurn: true,
-      // });
-
-    } else {
-      // 順番交代。this.state.gameSystem.isMyTurnが true → false → true → false になる。
-      gameSystem.isMyTurn = !gameSystem.isMyTurn;
-      // this.setState({ // 変更したstateをセットする
-      //   gameSystem.isMyTurn: !this.state.gameSystem.isMyTurn,
-      // });
-    }
-    // console.log("gameSystem.isMyTurnは、" + this.state.gameSystem.isMyTurn)
-
-    // 自分のターンでなければPC関数処理
-    if (!gameSystem.isMyTurn) {
-      await Util.sleep(2000);
-      Com.think()
-
-      await Util.sleep();
     }
   }
+  console.log("numWhiteは、" + numWhite + "枚")
+  console.log("numBlackは、" + numBlack + "枚")
+
+  let blackFlip = this.canFlip(PIECE.black); // 黒反転できるか否か // canFlip関数:盤面に引数の色の石を置けるかをbooleanで返す。
+  let whiteFlip = this.canFlip(PIECE.white); // 白反転できるか否か // canFlip関数:盤面に引数の色の石を置けるかをbooleanで返す。
+  const sumWhiteandBlack = numBlack + numWhite;
+
+  console.log("blackFlip: " + blackFlip)
+  console.log("whiteFlip: " + whiteFlip)
+  console.log("sumWhiteandBlack: " + sumWhiteandBlack)
+
+  if (sumWhiteandBlack === 64 || (!blackFlip && !whiteFlip)) { // 64手すべて打ち終わったときに
+    console.log("finish")
+    if (numWhite > numBlack) {
+      this.setState({
+        situation: "白の勝ち"
+      })
+    } else if (numWhite < numBlack) {
+      this.setState({
+        situation: "黒の勝ち"
+      })
+    } else {
+      this.setState({
+        situation: "引き分け"
+      })
+    }
+    return
+  }
+
+  console.log("gameSystem.isMyTurnは、" + gameSystem.isMyTurn)
+
+  // 石を置く順番を判定する処理
+  if (!blackFlip) {
+    this.showMessage("黒スキップ");
+    gameSystem.isMyTurn = false;
+    // this.setState({ // 変更したstateをセットする
+    //   gameSystem.isMyTurn: false,
+    // });
+
+  } else if (!whiteFlip) {
+    this.showMessage("白スキップ");
+    gameSystem.isMyTurn = true;
+    // this.setState({ // 変更したstateをセットする
+    //   gameSystem.isMyTurn: true,
+    // });
+
+  } else {
+    // 順番交代。this.state.gameSystem.isMyTurnが true → false → true → false になる。
+    gameSystem.isMyTurn = !gameSystem.isMyTurn;
+    // this.setState({ // 変更したstateをセットする
+    //   gameSystem.isMyTurn: !this.state.gameSystem.isMyTurn,
+    // });
+  }
+  // console.log("gameSystem.isMyTurnは、" + this.state.gameSystem.isMyTurn)
+
+  // 自分のターンでなければPC関数処理
+  if (!gameSystem.isMyTurn) {
+    await Util.sleep(2000);
+    // think()
+    useThink();
+
+    await Util.sleep();
+  }
+}
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
@@ -155,7 +157,7 @@ export default class Game extends React.Component {
     if (!gameSystem.isMyTurn === step % 2) {
       gameSystem.isMyTurn = true;
     }
-    // this.update();
+    // update();
   }
 
   // showMessage関数
